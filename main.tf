cab3c2a0f132cb98cdaeba6b2b0b0c1b8411d6b3
James Cooke <jamesedwardcooke@gmail.com>
2021-07-06 23:17:53 -0500

    Address apply inconsistencies

--- main.tf
+++ main.tf
@@ -24,15 +24,40 @@ data "kubectl_file_documents" "this" {
   content = data.flux_install.this.content
 }
 
+locals {
+  # Fix inconsistent result from cpu unit conversion on apply.
+  temp_documents_1 = [
+    for doc in data.kubectl_file_documents.this.documents :
+    yamldecode(replace(doc, "cpu: \"1000m\"", "cpu:\"1\""))
+  ]
+}
+
+locals {
+  # Remove out of spec "status" property in root of document.
+  temp_documents_2 = [
+    for doc in temp_documents_1 :
+    {
+      for key, value in doc :
+      key => value if !contains(["status"], key)
+    }
+  ]
+}
+
+locals {
+  documents = {
+    for doc in temp_documents_2 :
+    lower(
+      join("/", compact([doc.apiVersion, doc.kind, lookup(doc.metadata, "namespace", ""), doc.metadata.name]))
+    ) => doc
+  }
+}
+
 resource "kubernetes_manifest" "flux-install" {
   provider = kubernetes-alpha
 
-  for_each = toset(data.kubectl_file_documents.this.documents)
+  for_each = documents
 
-  manifest = {
-    for key, value in yamldecode(each.value) :
-    key => value if !contains(["status"], key)
-  }
+  manifest = each.value
 
   depends_on = [kubernetes_namespace.this]
 }
