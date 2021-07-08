e595c55e98d491e16823523ff7291dcf150882c9
James Cooke <jamesedwardcooke@gmail.com>
2021-07-07 23:00:28 -0500

    Last try

--- main.tf
+++ main.tf
@@ -64,7 +64,7 @@ resource "kubernetes_manifest" "this" {
 
   lifecycle {
     ignore_changes = [
-      manifest["metadata"]["creationTimestamp"],
+      manifest[0].metadata[0].creationTimestamp,
     ]
   }
 }
