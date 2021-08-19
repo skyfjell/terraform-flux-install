2bbfdefb4809f525d2ffd845339cd536265a5955
James Cooke <jamesedwardcooke@gmail.com>
2021-08-19 03:48:19 -0500

    Use main k8s provider for manifest resource

--- examples/basic/main.tf
+++ examples/basic/main.tf
@@ -1,11 +1,9 @@
 provider "kubernetes" {
   config_path    = "~/.kube/config"
   config_context = "k3d-tftest"
-}
-
-provider "kubernetes-alpha" {
-  config_path    = "~/.kube/config"
-  config_context = "k3d-tftest"
+  experiments {
+    manifest_resource = true
+  }
 }
 
 module "flux-install-basic" {
